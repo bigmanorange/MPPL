5695f4db956e46bd30fbba6c1b6437e06b933bd9
maahirvirsingh123-ctrl <maahirvirsingh123@gmail.com>
2026-03-01 16:32:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -23,3 +23,13 @@ export default defineConfig(({mode}) => {
     },
   };
 });
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
