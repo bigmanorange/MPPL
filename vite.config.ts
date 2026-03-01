c1ae2c64a4c49893f5a054ab40086b9f8c36c0f1
maahirvirsingh123-ctrl <maahirvirsingh123@gmail.com>
2026-03-01 16:45:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
 
   server: {
     host: true,
     port: 5173,
-  },
 
-  preview: {
-    host: true,
-    port: 5173,
-  },
-}));
+    // allow ngrok domains
+    allowedHosts: "all",
+
+    // THIS FIXES YOUR CONNECTION ERROR
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+});
