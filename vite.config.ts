d0e2b79d7f5fe242a0bc05a9da22f33939b5bb2d
maahirvirsingh123-ctrl <maahirvirsingh123@gmail.com>
2026-03-01 17:06:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,16 +7,16 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: [
-      "georgetta-renascent-doubly.ngrok-free.dev"
 
-    // THIS FIXES YOUR CONNECTION ERROR
+    // allow ngrok & tunnels
+    allowedHosts: "all",
+
     proxy: {
       "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+        secure: false,
+      },
+    },
+  },
 });
